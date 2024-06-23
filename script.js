d4b138270376dbff25b23218d5cc085af61eb6ae
Abeen Khanal <abeenkhanal16@gmail.com>
2024-06-23 17:04:38 +0545

    commited

--- script.js
+++ script.js
@@ -66,7 +66,8 @@ function navbar(active){
     navitems.forEach((li)=>{
         console.log(li.getAttribute('id')==active);
        li.classList.remove('active')
-       if(li.getAttribute('id')==active){
+    //    if(li.getAttribute('id')==active){
+    if(li.classList.contains(active)){
         li.classList.add('active')
        }
     })
